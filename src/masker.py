9f2cc3d6e9beb70b2a3e13e80f2ab82159fb8579
Zach F <zach@zkf.me>
2019-04-08 13:10:19 -0400

    Usability improvements

--- src/masker.py
+++ src/masker.py
@@ -2,23 +2,32 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-def infer(x, model, spacing):
-    masker = Masker(spacing, False)
-    acc = tf.zeros(x.shape)
+def infer(x, model, spacing=3):
+    masker = Masker(False, spacing)
+    result = tf.zeros(x.shape)
     
     for i in range(spacing * spacing):
         masked, mask = masker(x, i)
-        preds = model(masked)
+        masked = tf.reshape(masked,(-1, *x.shape[1:]))
+        predictions = model(tf.cast(masked, tf.float32))
     
-        acc = acc + tf.reshape(preds, x.shape) * tf.reshape(mask, x.shape)
-    return acc
+        result += tf.reshape(predictions, x.shape) * tf.reshape(mask, x.shape)
+    return result
 
 class Masker:
-    def __init__(self, spacing=3, interpolate=True):
+    def __init__(self, interpolate=True, spacing=3, radius=1, smooth=True):
         self.spacing = spacing
         self.interpolate = interpolate
         
-        kernel = np.array([[0.5, 1.0, 0.5], [1.0, 0.0, 1.0], (0.5, 1.0, 0.5)])
+        # build a disk-shaped matrix to convolve with.
+        x, y = np.ogrid[-radius:(radius+1), -radius:(radius+1)]
+        within_outer_radius = (x*x + y*y <= (radius+1)*(radius+1))
+        within_inner_radius = (x*x + y*y <= radius*radius)
+        kernel = np.zeros((2*radius + 1, 2*radius + 1))
+        if smooth:
+            kernel[within_outer_radius] = 0.5 # fill in outer disk 
+        kernel[within_inner_radius] = 1.0 # fill in inner disk 
+        kernel[radius,radius] = 0.0 # subtract center point
         kernel /= kernel.sum()
         self.interpolate_kernel = kernel[:, :, np.newaxis, np.newaxis]
     
@@ -32,7 +41,7 @@ class Masker:
         if self.interpolate:
             masked = self._apply_interpolate_mask(x, mask, mask_inv)
         else:
-            masked = tf.cast(x, mask_inv.dtype) * tf.reshape(mask_inv,x.shape)
+            masked = x * tf.cast(tf.reshape(mask_inv, x.shape), x.dtype)
     
         return masked, mask
     
@@ -42,7 +51,8 @@ class Masker:
             for j in range(shape[2]):
                 if (i % self.spacing == phase_x and j % self.spacing == phase_y):
                     grid[i, j] = 1
-        return tf.constant(np.ones([shape[0], 1, 1]) * grid, dtype=tf.float32)
+        grid = np.ones([shape[0], 1, 1]) * grid
+        return tf.constant(grid, dtype=tf.float32)
     
     def _apply_interpolate_mask(self, x, mask, mask_inv):
         x64 = tf.cast(x, tf.float64)
@@ -50,6 +60,4 @@ class Masker:
         filtered = tf.nn.conv2d(x64, kernel, strides=[1,1,1,1], padding='SAME')
         x_squeezed = tf.dtypes.cast(tf.squeeze(x64), dtype=mask.dtype)
         filtered_squeezed = tf.dtypes.cast(tf.squeeze(filtered), dtype=tf.float32)
-        term1 = filtered_squeezed * mask
-        term2 = x_squeezed  *  mask_inv
-        return term1 + term2
+        return (filtered_squeezed * mask) + (x_squeezed * mask_inv)
